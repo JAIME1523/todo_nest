fa9d6819aa07b896359ddbe67b4ca8541806bef2
jaime1523 <jaime5vivar55@gmail.com>
2024-12-08 21:01:27 -0600

    Querys

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
 import { HelloWorldModule } from './hello-world/hello-world.module';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 
 @Module({
   imports:  [
@@ -11,6 +12,10 @@ import { HelloWorldModule } from './hello-world/hello-world.module';
     // debug: false,
     // playground: false
     autoSchemaFile: join( process.cwd(), 'src/schema.gql'),
+    playground: false,
+plugins: [
+ApolloServerPluginLandingPageLocalDefault()
+]
     }),
     HelloWorldModule,
     ],
--- src/hello-world/hello-world.resolver.ts
+++ src/hello-world/hello-world.resolver.ts
@@ -1,14 +1,22 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Float, Int, Query, Resolver } from '@nestjs/graphql';
+import { randomInt } from 'crypto';
 
 @Resolver()
 export class HelloWorldResolver {
 
-    @Query(()=> String) 
+    @Query(()=> String, {description: "Hola es lo que regresa", name:'hello'}) 
     helloWorld():String{
         return "Hola mundo"
     }
-    @Query(()=> String) 
-    helloWorld2():String{
-        return "Hola mundo2"
+    @Query(()=> Float,{name:'ramdonNumber'}) 
+    getNumber():number{
+        var valor  = Math.random()*100;
+        return valor;
+    }
+
+    @Query(()=> Int,{name:'ramdonFronZeroTo', description :"from zero to arugument"}) 
+    getRamdonFronZeroTO(@Args('to',{type:()=> Int, nullable: true})  to: number= 6):number {
+        return Math.floor(Math.random() * to);
+       
     }
 }
